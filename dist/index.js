c4613b461f8331d5e9a6ad276e52d9812f42792c
sandeep-paliwal <spaliwal@adobe.com>
2020-02-28 20:19:58 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -57,23 +57,13 @@ const core = __webpack_require__(470)
 const execSync = __webpack_require__(129).execSync
 
 try {
-  const namespace = core.getInput('AIO_RUNTIME_NAMESPACE');
-  const auth = core.getInput('AIO_RUNTIME_AUTH')
-  
-  if(!namespace || !auth)
-    throw new Error("AIO_RUNTIME_NAMESPACE and AIO_RUNTIME_AUTH must be passed to the action")
-
-  process.env.AIO_RUNTIME_NAMESPACE = namespace
-  process.env.AIO_RUNTIME_AUTH = auth
-
   let options = {
       stdio: 'inherit',
       env: process.env
   }
-  execSync('sudo --preserve-env aio app deploy --skip-build', options)
-
+   execSync('aio app deploy --skip-deploy', options)
 } catch (error) {
-  core.setFailed(error.message)
+  core.setFailed(error.message);
 }
 
 
